56df67101d4ae65476bf999fcd62b49b1de56d8f
Adrian Guerrero <aguerrerocastro2003@gmail.com>
2025-11-20 13:16:53 +0100

    actualizacion del hud

--- Scripts/Hud.gd
+++ Scripts/Hud.gd
@@ -1,25 +1,21 @@
 extends CanvasLayer
 
-@onready var health_text: Label = $LeftContainer/VBoxContainer/HealthText
 @onready var health_bar: ProgressBar = $LeftContainer/VBoxContainer/HealthBar
-@onready var ammo_text: Label = $RightContainer/AmmoRow/AmmoText
 @onready var ammo_value: Label = $RightContainer/AmmoRow/AmmoValue
 
 
 
 func _ready() -> void:
 	health_bar.min_value = 0
-	ammo_text.text = "Munición:"
 	ammo_value.text = "--"
 
 	# Registrarse en el GameManager
 	GameManager.register_hud(self)
 
 
-func update_health(current: int, max: int) -> void:
-	health_bar.max_value = max
+func update_health(current: int, max_health: int) -> void:
+	health_bar.max_value = max_health
 	health_bar.value = current
-	#health_text.text = "Health: %d / %d" % [current, max]
 
 
 func set_has_gun(has_gun: bool) -> void:
@@ -29,8 +25,8 @@ func set_has_gun(has_gun: bool) -> void:
 		ammo_value.text = "--"
 
 
-func update_ammo(current: int, max: int) -> void:
-	if max > 0:
-		ammo_value.text = "%d / %d" % [current, max]
+func update_ammo(current: int, max_ammo: int) -> void:
+	if max_ammo > 0:
+		ammo_value.text = "%d / %d" % [current, max_ammo]
 	else:
 		ammo_value.text = "--"
